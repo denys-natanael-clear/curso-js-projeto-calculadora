5a897876613309034e7c7e28e6330d69a1b59433
denys-natanael-clear <denys.natanael.clear@gmail.com>
2023-02-15 09:04:16 -0300

    lessons 14 and 15: adding the numbers in the calculator with the switch case, applying the methods for the validation of numbers and arithmetic signs

--- scripts/controllers/calcController.js
+++ scripts/controllers/calcController.js
@@ -2,6 +2,7 @@ class CalcController {
 
     constructor () {
 
+        this._operation = []
         this._locale = 'pt-BR'
         this._displayCalEl = document.querySelector('#display')
         this._dateEl = document.querySelector('#data')
@@ -32,7 +33,123 @@ class CalcController {
         })
 
     }
+
+    clearAll () {
+
+        this._operation = []
+
+    }
+
+    clearEntry (value) {
+
+        this._operation.pop(value)
+
+    }
+
+    getLastOperation () {
+
+        return this._operation[this._operation.length-1]
+
+    }
+
+    setLastOperation (value) {
+
+        this._operation[this._operation.length-1] = value
+
+    }
+
+    isOperator (value) {
+
+        return (['+', '-', '*', '%', '/'].indexOf(value) > -1)
+
+    }
+
+    addOperation (value) {
+
+        if(isNaN(this.getLastOperation(value))) {
+            
+            if(this.isOperator(value)) {
+                
+                this.setLastOperation = value
+
+            } else if (isNaN(value)) { 
+
+                console.log(value)
+
+            } else {
+
+                this._operation.push(value)
+
+            }
+
+        } else {
+            let newValue = this.getLastOperation().toString() + value.toString()
+            this.setLastOperation(parseInt(newValue))
+        }
+
+        console.log(this._operation)
+
+    }
+
+    setError () {
+
+        this.displayCalc = "Error"
+
+    }
     
+    
+
+    execBtn (value) {
+
+        switch (value) {
+
+            case 'ac':
+                this.clearAll()
+                break
+            case 'ce':
+                this.clearEntry()
+                break
+            case 'soma':
+                this.addOperation('+')
+                break
+            case 'subtração':
+                this.addOperation('-')
+                break
+            case 'divisão':
+                this.addOperation('/')
+                break
+            case 'multiplicação':
+                this.addOperation('*')
+                break
+            case 'porcento':
+                this.addOperation('%')
+                break
+            case 'igual':
+                
+                break
+            case 'ponto':
+                this.addOperation('.')
+                break
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value))
+            break
+            default:
+                this.setError()
+                break
+
+        }
+
+    }
 
     initButtonsEvents () {
 
@@ -40,11 +157,14 @@ class CalcController {
 
         buttons.forEach((btn, index) => {
 
-            this.addEventListenerAll(btn, 'click drag mouseover', e => {
+            this.addEventListenerAll(btn, 'click drag', e => {
 
-                console.log(btn.className.baseVal.replace("btn-",""))
+                let textBtn = btn.className.baseVal.replace("btn-","")
+
+                this.execBtn(textBtn)
     
             })
+
 
             this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
                 
