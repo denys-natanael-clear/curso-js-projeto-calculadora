623f97d9777c472a7cf599cedde53638c5eb6bee
denys-natanael-clear <denys.natanael.clear@gmail.com>
2023-02-15 11:07:22 -0300

    feat: lectures 16, 17, 18, 19: applying the methods of calculation functionalities such as: addition, subtraction, division, percentage, multiplication, equal button and showing on the display.

--- scripts/controllers/calcController.js
+++ scripts/controllers/calcController.js
@@ -2,6 +2,9 @@ class CalcController {
 
     constructor () {
 
+        this._lastOperator = ''
+        this._lastNumber = ''
+
         this._operation = []
         this._locale = 'pt-BR'
         this._displayCalEl = document.querySelector('#display')
@@ -22,6 +25,7 @@ class CalcController {
 
         }, 1000);
         
+        this.setLastNumberToDisplay()
     }
 
     addEventListenerAll(element, events, fn) {
@@ -38,11 +42,15 @@ class CalcController {
 
         this._operation = []
 
+        this.setLastNumberToDisplay()
+
     }
 
     clearEntry (value) {
 
         this._operation.pop(value)
+
+        this.setLastNumberToDisplay()
 
     }
 
@@ -64,30 +72,142 @@ class CalcController {
 
     }
 
+    pushOperation (value) {
+
+        this._operation.push(value)
+
+        if (this._operation.length > 3) {
+
+            this.calc()
+
+        }
+
+    }
+
+    getResult () {
+
+
+
+        return eval(this._operation.join(""))
+
+    }
+
+    calc () {
+
+        let last = ''
+
+        this._lastOperator = this.getLastItem()
+
+        if(this._operation.length < 3) {
+
+            let fistItem = this._operation[0]
+            this._operation = [fistItem, this._lastOperator, this._lastNumber]
+
+        }
+
+        if (this._operation.length > 3) {
+            last = this._operation.pop()
+
+            this._lastNumber = this.getResult()
+
+        } else if (this._operation.length == 3) {
+
+            this._lastNumber = this.getLastItem(false)
+
+        }
+
+        console.log('_lastOperator', this._lastOperator)
+        console.log('_lastNumber', this._lastNumber)
+
+        let result = this.getResult()
+
+        if (last == '%') {
+
+            result /= 100
+
+            this._operation = [result]
+
+        } else {
+
+            this._operation = [result]
+
+            if (last) this._operation.push(last)
+
+        }
+
+        this.setLastNumberToDisplay()
+
+    }
+
+    getLastItem (isOperator = true) {
+
+        let lastItem
+        
+        for (let i = this._operation.length -1; i >= 0; i--) {
+            
+            if (this.isOperator(this._operation[i]) == isOperator) {
+                lastItem = this._operation[i]
+                break
+            }
+
+        }
+
+        if (!lastItem) {
+        
+            lastItem = (isOperator) ? this._lastOperator : this._lastNumber
+
+        }
+
+        return lastItem
+
+    }
+
+    setLastNumberToDisplay () {
+
+        let lastNumber = this.getLastItem(false)
+
+        if(!lastNumber) lastNumber = 0
+
+        this.displayCalc = lastNumber
+
+    }
+
     addOperation (value) {
 
-        if(isNaN(this.getLastOperation(value))) {
+        if (isNaN(this.getLastOperation(value))) {
             
-            if(this.isOperator(value)) {
+            if (this.isOperator(value)) {
                 
-                this.setLastOperation = value
+                this.setLastOperation(value)
 
-            } else if (isNaN(value)) { 
+            } else if(isNaN(value)) {   
 
-                console.log(value)
+                console.log('Outra Coisa', value)
 
             } else {
 
-                this._operation.push(value)
+                this.pushOperation(value)
+
+                this.setLastNumberToDisplay()
 
             }
 
         } else {
-            let newValue = this.getLastOperation().toString() + value.toString()
-            this.setLastOperation(parseInt(newValue))
-        }
 
-        console.log(this._operation)
+            if(this.isOperator (value)) {
+
+                this.pushOperation(value)
+
+            } else {
+
+                let newValue = this.getLastOperation().toString() + value.toString()
+                this.setLastOperation(parseInt(newValue))
+
+                this.setLastNumberToDisplay()
+
+            }
+            
+        }
 
     }
 
@@ -112,20 +232,20 @@ class CalcController {
             case 'soma':
                 this.addOperation('+')
                 break
-            case 'subtração':
+            case 'subtracao':
                 this.addOperation('-')
                 break
-            case 'divisão':
+            case 'divisao':
                 this.addOperation('/')
                 break
-            case 'multiplicação':
+            case 'multiplicacao':
                 this.addOperation('*')
                 break
             case 'porcento':
                 this.addOperation('%')
                 break
             case 'igual':
-                
+                this.calc()
                 break
             case 'ponto':
                 this.addOperation('.')
